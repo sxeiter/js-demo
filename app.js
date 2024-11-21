14dbacd8dccf5077dd0ba3a1cf95b43bbdff8eb1
sxeiter <sxeiter@mail.ru>
2024-11-21 23:34:30 +0500

    дз по массивам

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-function currencyConverter(amount, fromCurrency, toCurrency) {
-    let rate;
+function encryptPassword(password) {
+    const passwordArray = password.split('');
+    const length = passwordArray.length;
+    const encryptedArray = new Array(length);
 
-    if (fromCurrency === 'RUB' && toCurrency === 'USD') {
-        rate = 0.03;
-    } else if (fromCurrency === 'RUB' && toCurrency === 'EUR') {
-        rate = 0.028;
-    } else if (fromCurrency === 'USD' && toCurrency === 'RUB') {
-        rate = 33.33;
-    } else if (fromCurrency === 'USD' && toCurrency === 'EUR') {
-        rate = 0.93;
-    } else if (fromCurrency === 'EUR' && toCurrency === 'RUB') {
-        rate = 35.71;
-    } else if (fromCurrency === 'EUR' && toCurrency === 'USD') {
-        rate = 1.08;
-    } else {
-        return null; 
+    for (let i = 0; i < length; i++) {
+        if (i < length / 2) {
+            encryptedArray[i] = passwordArray[length - 1 - i]; .
+        } else {
+            encryptedArray[i] = passwordArray[i]; 
+        }
     }
-    return amount * rate;
+    return encryptedArray.join('');
 }
-// через switch не стал делать)
+
+function checkPassword(originalPassword, encryptedPassword) {
+    const decryptedPassword = encryptPassword(encryptedPassword); 
+    return originalPassword === decryptedPassword; 
+}
+
