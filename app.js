bb240ffb338a9a1d02085e1fd5092c04c50cefd8
sxeiter <sxeiter@mail.ru>
2024-11-21 22:46:40 +0500

    дз по функциям

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const hasLicence = true;
-const age = 23;
-const isDrunk = false;
-const canDrive = (age >= 18 && hasLicence && !isDrunk);
+function currencyConverter(amount, fromCurrency, toCurrency) {
+    let rate;
 
-
-console.log(`Вы ${canDrive ? 'можете' : 'не можете'} сесть за машину? `)
+    if (fromCurrency === 'RUB' && toCurrency === 'USD') {
+        rate = 0.03;
+    } else if (fromCurrency === 'RUB' && toCurrency === 'EUR') {
+        rate = 0.028;
+    } else if (fromCurrency === 'USD' && toCurrency === 'RUB') {
+        rate = 33.33;
+    } else if (fromCurrency === 'USD' && toCurrency === 'EUR') {
+        rate = 0.93;
+    } else if (fromCurrency === 'EUR' && toCurrency === 'RUB') {
+        rate = 35.71;
+    } else if (fromCurrency === 'EUR' && toCurrency === 'USD') {
+        rate = 1.08;
+    } else {
+        return null; 
+    }
+    return amount * rate;
+}
