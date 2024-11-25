9ba5c2304e0fdcfd89fa7403d1e939eb72e69add
sxeiter <sxeiter@mail.ru>
2024-11-25 13:43:22 +0500

    переделал домашку

--- app.js
+++ app.js
@@ -18,3 +18,26 @@ function checkPassword(originalPassword, encryptedPassword) {
     return originalPassword === decryptedPassword; 
 }
 
+function crypto(password) {
+    if (!password || password.length < 8 || password.length > 10) {
+        return null;
+    }
+    const middleIndex = Math.floor(password.length / 2);
+    const firstHalf = password.slice(0, middleIndex);
+    const secondHalf = password.slice(middleIndex);
+    const reversedSecondHalf = secondHalf.split('').reverse().join('');
+    const swappedFirstAndMiddle = firstHalf.slice(0, 1) + reversedSecondHalf.slice(3, 4) + firstHalf.slice(0, 1) + reversedSecondHalf.slice(0, 3) + firstHalf.slice(2);
+    return swappedFirstAndMiddle;
+}
+
+function checkPassword(encryptedPassword, originalPassword) {
+    if (!encryptedPassword || !originalPassword) {
+        return false;
+    }
+    return originalPassword === crypto(encryptedPassword);
+}
+
+const password = 'userpassword';
+const encryptedPassword = crypto(password);
+const decryptedPassword = crypto(encryptedPassword);
+console.log(password, encryptedPassword, decryptedPassword, password === decryptedPassword);
