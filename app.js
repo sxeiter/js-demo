71caf22228c106c722e34b9a5da3e2be7c035346
sxeiter <sxeiter@mail.ru>
2024-11-28 22:48:49 +0500

    сделал дз по this

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
-function createQuery(params) {
-    const urlParams = Object.entries(params).map(([key, value]) => `${key}=${value}`);
-    return urlParams.join('&');
+'use strict'
+
+let ToDoList = {
+  tasks: [],
+  currentId: 0, 
+  
+  addTask(title, priority) {
+    const newTask = {
+      title,
+      id: ++this.currentId, 
+      priority
+    };
+    
+    this.tasks.push(newTask);
+    return newTask;
+  },
+  
+  deleteTask(id) {
+    this.tasks = this.tasks.filter(task => task.id !== id);
+  },
+  
+  updateTask(id, updates) {
+    for (let i = 0; i < this.tasks.length; i++) {
+      if (this.tasks[i].id === id) {
+        Object.assign(this.tasks[i], updates);
+        break;
+      }
+    }
+  },
+  
+  sortTasks(field, order = 'asc') {
+    const direction = order === 'desc' ? -1 : 1;
+    
+    this.tasks.sort((a, b) => {
+      if (a[field] < b[field]) return -direction;
+      if (a[field] > b[field]) return direction;
+      return 0;
+    });
+  }
 };
-
-const newObj = {
-    search: 'Вася',
-    surname: 'пупкин',
-    take: 10,
-};
-
-console.log(createQuery(newObj));
-
